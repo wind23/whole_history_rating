7614a5d3d91b9e9d8fb3a5cf0107a8e05217da29
Tianyi Hao <haotianyi0@126.com>
2022-01-16 05:38:31 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
 from distutils.core import setup
 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding='utf-8')
+
 setup(
     name='whr',
     packages=['whr'],
-    version='0.0.1',
+    version='1.0.0',
     license='MIT',
     description=
     'A Python implementation of the Whole History Rating algorithm proposed by Rémi Coulom. '
     'The implementation is based on the Ruby code of GoShrine.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Tianyi Hao',
     author_email='haotianyi0@126.com',
     url='https://github.com/wind23/whole_history_rating',
     download_url=
-    'https://github.com/wind23/whole_history_rating/archive/0.0.1.tar.gz',
+    'https://github.com/wind23/whole_history_rating/archive/1.0.0.tar.gz',
     keywords=['WHR', 'whole history rating', 'Elo rating'],
     install_requires=[],
     classifiers=[
